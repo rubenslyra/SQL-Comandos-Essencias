197682b7be227eaead64d8be7347e67aace42483
Rubens Lyra <rubensvlyra@gmail.com>
2023-01-09 22:07:57 -0300

    Update Criando e Alterando a Storage Procedure - DTL .sql

--- Criando e Alterando a Storage Procedure - DTL .sql
+++ Criando e Alterando a Storage Procedure - DTL .sql
@@ -4,15 +4,16 @@ CREATE PROCEDURE input_data()
 BEGIN 
 DECLARE error_sql TINYINT DEFAULT FALSE;
 DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET error_sql = TRUE;
-START TRANSACTION;
-	delete from db_escola.alunos_novos;
-	CHANGE COLUMN idAluno idAluno int auto_increment primary key;
-	insert into alunos(MAT, Nome, Email)
+START TRANSACTION;	
+	insert into alunos_novo(MAT, Nome, Email)
 	values 
 	(1, 'João Silva', 'joao@email.com'),
 	(2, 'Carmen Miranda', 'carmen@eumail.com'),
 	(3, 'Jorge Amado', 'jorge@eumail.com'),
-	(4, 'Maria Quitéria', 'maria@eumail.com');
+	(4, 'Maria Quitéria', 'maria@eumail.com'),
+    (5, 'Aderbal Cezar', 'aderbal@email.com'),
+	(6, 'John Paul', 'john@email.com'),
+	(7, 'Paul Gilbert', 'test3@email.com');
 	IF error_sql = FALSE THEN
 		COMMIT;
         SELECT "Operação realizada com sucesso!" AS Response;
@@ -20,8 +21,8 @@ START TRANSACTION;
 		ROLLBACK;
         SELECT "Alguns erros forma encontrados e a operação foi cancelada." AS Response;
 	END IF;
-END %$    
-DELIMITER ; -- Atenção ao espaço entre a declaração DELIMITER e o ';'.alter
+END %$
+DELIMITER ; 
 
 
 -- Para Alterar
